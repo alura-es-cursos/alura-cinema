b76a1e1c0e27f54e32568673fb9d09d24a8365e4
Daniel Riverol <34141421+DanielRiverol@users.noreply.github.com>
2023-12-18 13:35:41 -0300

    Revert "Aula 2"

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,16 +1,11 @@
 import Cabecera from "components/Cabecera/Cabecera";
 import Pie from "components/Pie";
-import Banner from "components/Banner";
-import Titulo from "components/Titulo";
 
 function Inicio() {
   return (
     <>
       <Cabecera></Cabecera>
-      <Banner img="home" color="#154580"/>
-      <Titulo>
-        <h1>Un lugar para guardar sus videos favoritos </h1>
-      </Titulo>
+     
       <Pie />
     </>
   );
